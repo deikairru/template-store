a8348bf2419ca4176e4d3a34bd3f6018f97c4d7c
deikairru <deikairru@gmail.com>
2024-09-07 13:05:20 +0700

    feat(api): connect database, and route api to server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js"
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, ()=> {
+app.use(express.json())
+
+app.use("/api/products", productRoutes);
+
+app.listen(5000, () => {
+  connectDB();
   console.log("Server started at port 5000")
 })
