4fd4e31e0bfe3ef13c3eba64abceb755f80bca07
deikairru <deikairru@gmail.com>
2024-09-08 14:12:55 +0700

    chore: init frontend react tailwind using vite

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './pages/**/*.{js,jsx}',
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
